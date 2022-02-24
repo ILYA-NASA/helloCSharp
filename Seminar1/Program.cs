80a84665bc2f809d05a2f022d42495b971c64c0a
ILYA <IL.NASA@OUTLOOK.COM>
2022-02-24 11:03:49 +0300

    1 exercise done

---
+++ Seminar1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите целое число: ");
+int value = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat_value = value*value;
+
+Console.WriteLine("Квадрат данного числа равен");
+Console.Write(kvadrat_value);
