e17530390a7da96be205e8a84cc44abdae1ba8ac
ILYA <IL.NASA@OUTLOOK.COM>
2022-02-24 12:28:02 +0300

    2 3 and 4 exercise done

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -1,7 +1,55 @@
-﻿Console.WriteLine("Введите целое число: ");
-int value = Convert.ToInt32(Console.ReadLine());
+﻿/*
+//2 задача
 
-int kvadrat_value = value*value;
+Console.WriteLine("Введите первое целое число: ");
+int value1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе целое число: ");
+int value2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Квадрат данного числа равен");
-Console.Write(kvadrat_value);
+int kvadrat_value2 = value2 * value2;
+
+if(kvadrat_value2 == value1)
+{
+    Console.WriteLine("Квадрат второго числа сооветствует первому");
+}
+else
+{
+    Console.WriteLine("Искомых соответствийне не выявлено");
+}
+*/
+
+/*
+// 3 задача
+
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number <= 7 && number >= 1)
+{
+    if(number == 1)
+    {
+        Console.WriteLine("Понедельник");
+    }
+        if(number == 2)
+    {
+        Console.WriteLine("Вторник");
+    }
+
+}
+else
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
+*/
+
+//4 задача
+
+Console.Write("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int neg_n = n*(-1);
+
+while(neg_n <= n)
+{
+    Console.WriteLine(neg_n);
+    neg_n++;
+}
