b9e4c8042926f126d559be2aa0052cd844a68691
ILYA <IL.NASA@OUTLOOK.COM>
2022-02-27 15:51:01 +0300

    Exercise1 for HomeWerk1 is done

---
+++ homewerk1/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача №1
+Console.WriteLine("Введите первое целое число: ");
+int value_a = Convert.ToInt32(Console.ReadLine());//5, 2, -9
+Console.WriteLine("Введите второе целое число: ");
+int value_b = Convert.ToInt32(Console.ReadLine());//7,10, -3
+
+if (value_a > value_b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(value_a);
+    Console.Write("min = ");
+    Console.Write(value_b);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(value_b);
+    Console.Write("min = ");
+    Console.Write(value_a);
+}
