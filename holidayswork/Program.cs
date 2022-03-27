2f19eada50b79cd5ad72ef529d56155cc166ef30
ILYA <IL.NASA@OUTLOOK.COM>
2022-03-27 16:45:11 +0300

    Renamed folders

---
+++ holidayswork/Program.cs
@@ -0,0 +1,56 @@
+﻿//Задача 1
+//Пользователь вводит целые числа до тех пор, пока не введёт ноль. 
+//Вывести на экран первые два максимальных и первые два минимальных числа из ряда введенных чисел. 
+
+void toZero(int max1, int max2, int min1, int min2)
+{
+    Console.WriteLine("Введите еще одно любое целое число (теперь можете ввести 0 для завершения ввода): ");
+    int NextNumber = Convert.ToInt32(Console.ReadLine());
+    if (NextNumber == 0)
+    {
+        Console.WriteLine("Первое максимальное число: " + max1 + " Второе максимальное число: " + max2);
+        Console.WriteLine("Первое минимальное число: " + min1 + " Второе минимальное число: " + min2);
+    }
+    else
+    {
+        if (NextNumber > max1)
+        {
+            if (max2 > max1)
+            {
+                max1 = max2;
+                max2 = NextNumber;
+            }
+            else max2 = NextNumber;
+        }
+        if (NextNumber > max2) max2 = NextNumber;
+        if (NextNumber < min1)
+        {
+            if (min2 < min1)
+            {
+                min1 = min2;
+                min2 = NextNumber;
+            }
+            else min2 = NextNumber;
+        }
+        if (NextNumber < min2) min2 = NextNumber;
+        toZero(max1, max2, min1, min2);
+    }
+}
+
+Console.WriteLine("Введите любое целое число КРОМЕ 0: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number == 0) Console.WriteLine("ЗДЕСЬ НЕ НУЖНО ВВОДИТЬ 0! Попробуйте еще раз, когда захотите следовать инструкциям.");
+else
+{
+    Console.WriteLine("Введите еще одно любое целое число КРОМЕ 0: ");
+    int NewNumber = Convert.ToInt32(Console.ReadLine());
+    if (NewNumber == 0) Console.WriteLine("ЗДЕСЬ НЕ НУЖНО ВВОДИТЬ 0! Попробуйте еще раз, когда захотите следовать инструкциям.");
+    else
+    {
+        int MAX1 = Number;
+        int MAX2 = NewNumber;
+        int MIN1 = Number;
+        int MIN2 = NewNumber;
+        toZero(MAX1, MAX2, MIN1, MIN2);
+    }
+}
---
+++ homework2/Program.cs
@@ -0,0 +1,78 @@
+﻿/*
+//Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+void SecNum(int num)
+{
+    int n1 = num % 10;
+    int n2 = num / 100 * 100;
+    int SecN = (num - n2 - n1) / 10;
+    Console.WriteLine($"Current number is {num}, Second number is {SecN}");
+}
+
+Console.WriteLine("Введите натуральное трехзначное число: "); //456, 782, 918 
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 999 || number <= 99) Console.WriteLine("Вы ввели не трехзначное число или введенное число не натуральное, повторите попытку.");
+else SecNum(number);
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного 
+//числа или сообщает, что третьей цифры нет.
+
+// Решние 1
+
+Console.WriteLine("Введите целое число: "); //645, 78, 32679
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 99 && number >= -99)
+{
+    Console.WriteLine("Tретьей цифры нет");
+}
+else
+{
+    if (number <= 999 && number >= -999)
+    {
+        int ThirdNum = number % 10;
+        Console.WriteLine("Current number is " + number + ", Third Number is " + ThirdNum);
+    }
+    if (number <= 9999 && number >= -9999)
+    {
+        int ThirdNum1 = number / 10;
+        int ThirdNum2 = ThirdNum1 % 10;
+        Console.WriteLine("Current number is " + number + ", Third Number is " + ThirdNum2);
+    }
+    if (number <= 99999 && number >= -99999)
+    {
+        int ThirdNum1 = number / 100;
+        int ThirdNum2 = ThirdNum1 % 10;
+        Console.WriteLine("Current number is " + number + ", Third Number is " + ThirdNum2);
+    }
+}
+
+// Решение 2 (с использованием ператора String)
+Console.WriteLine("Enter your number:");
+string num = Console.ReadLine();
+if (num.Length < 3) Console.WriteLine("There is no 3rd part");
+else Console.WriteLine(num[2]);
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 7 && number >= 1)
+{
+    if (number <= 5 && number >= 1)
+    {
+        Console.WriteLine("Будний день");
+    }
+    if (number <= 7 && number >= 5)
+    {
+        Console.WriteLine("Выходной");
+    }
+}
+else
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
+*/
