49557db3722d9e6c25f767858cf67fdb35c07750
ILYA <IL.NASA@OUTLOOK.COM>
2022-04-01 21:32:27 +0300

    Exercises for Seminar 7 is done

---
+++ Seminar7/Program.cs
@@ -0,0 +1,87 @@
+﻿// Задача 1
+// Задайте двумерный массив размером m×n, 
+// заполненный случайными целыми числами.
+
+// void RandomArray(int m, int n)
+// {
+//     int[,] matrix = new int[m, n];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(-99, 100);
+//             Console.Write($"{matrix[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// RandomArray(4, 4);
+
+// Задача 2
+// Задайте двумерный массив размера m на n, 
+// каждый элемент в массиве находится по формуле: Aₘₙ = m+n. 
+// Выведите полученный массив на экран.
+
+// void SumArray(int m, int n)
+// {
+//     int[,] matrix = new int[m, n];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = i + j;
+//             Console.Write($"{matrix[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// SumArray(4, 4);
+
+// Задача 3
+// Задайте двумерный массив. 
+// Найдите элементы, у которых оба индекса чётные, 
+// и замените эти элементы на их квадраты.
+
+// void NewRandomArray(int m, int n)
+// {
+//     int[,] matrix = new int[m, n];
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             matrix[i, j] = new Random().Next(2, 3);
+//             if (i % 2 == 0 && j % 2 == 0) 
+//             matrix[i, j] = matrix[i, j] * matrix[i, j];
+//             Console.Write($"{matrix[i, j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// NewRandomArray(5, 5);
+
+// Задача 4
+// Задайте двумерный массив. 
+// Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.)
+
+void NewSumArray(int m, int n)
+{
+    int[,] matrix = new int[m, n];
+    int SumArray = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 10);
+            if (i == j) SumArray = SumArray + matrix[i, j];
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine($"Сумма элементов по диагонали равна: {SumArray}");
+}
+
+NewSumArray(5, 5);
